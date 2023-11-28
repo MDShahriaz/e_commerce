97a7b801a584577a5125836b47e28bc1bb92f2c9
Shahriazshuvo <Shahriaz.Montakim@brainstation-23.com>
2023-11-28 20:35:51 +0600

    design custom appbar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,21 +1,27 @@
-import 'package:e_commerce/common/widgets/custom_shapes/containers/circular_container.dart';
-import 'package:e_commerce/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
-import 'package:e_commerce/common/widgets/custom_shapes/curved_edges/curverd_edge_widget.dart';
-import 'package:e_commerce/utils/device/device_utility.dart';
+import 'package:e_commerce/common/widgets/appbar/appbar.dart';
 import 'package:flutter/material.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
 import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/text_strings.dart';
+import 'widgets/home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TPrimaryHeaderContainer(child: Container()),
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  THomeAppBar(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
