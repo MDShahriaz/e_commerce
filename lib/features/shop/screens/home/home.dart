16a82220ce52fa48b9756e39b397cc5a79366b5f
Shahriazshuvo <Shahriaz.Montakim@brainstation-23.com>
2023-12-02 20:43:31 +0600

    add promo slider in home page

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,12 +1,13 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:e_commerce/features/shop/screens/home/widgets/home_categories.dart';
-import 'package:e_commerce/utils/constants/image_strings.dart';
-import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:e_commerce/features/shop/screens/home/widgets/promo_slider.dart';
 import 'package:flutter/material.dart';
+import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
-import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
+import '../../../../common/widgets/images/t_rounded_image.dart';
 import '../../../../common/widgets/texts/section_heading.dart';
-import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/image_strings.dart';
 import '../../../../utils/constants/sizes.dart';
 import 'widgets/home_appbar.dart';
 
@@ -54,68 +55,16 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(TSizes.defaultSpace),
-              child: TRoundedImage(
-                imageUrl: TImages.promoBanner3,
+              padding: EdgeInsets.all(TSizes.defaultSpace),
+              child: TPromoSlider(
+                banners: [
+                  TImages.promoBanner1,
+                  TImages.promoBanner2,
+                  TImages.promoBanner3
+                ],
               ),
-            )
+            ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class TRoundedImage extends StatelessWidget {
-  const TRoundedImage({
-    super.key,
-    this.width,
-    this.height,
-    required this.imageUrl,
-    this.applyImageRadius = false,
-    this.border,
-    this.backgroundColor = TColors.light,
-    this.fit,
-    this.padding,
-    this.isNetworkImage = false,
-    this.onPressed,
-    this.borderRadius = TSizes.md,
-  });
-
-  final double? width, height;
-  final String imageUrl;
-  final bool applyImageRadius;
-  final BoxBorder? border;
-  final Color backgroundColor;
-  final BoxFit? fit;
-  final EdgeInsetsGeometry? padding;
-  final bool isNetworkImage;
-  final VoidCallback? onPressed;
-  final double borderRadius;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        width: width,
-        height: height,
-        padding: padding,
-        decoration: BoxDecoration(
-          borderRadius: applyImageRadius
-              ? BorderRadius.circular(borderRadius)
-              : BorderRadius.zero,
-        ),
-        child: ClipRRect(
-          borderRadius: applyImageRadius
-              ? BorderRadius.circular(borderRadius)
-              : BorderRadius.zero,
-          child: Image(
-            image: isNetworkImage
-                ? NetworkImage(imageUrl)
-                : AssetImage(imageUrl) as ImageProvider,
-            fit: fit,
-          ),
         ),
       ),
     );
