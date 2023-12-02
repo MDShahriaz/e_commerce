e45f3f5cb461dd95d7e57dec8eca7f32c95d1df3
Shahriazshuvo <Shahriaz.Montakim@brainstation-23.com>
2023-12-02 18:30:26 +0600

    change git remotes

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -15,7 +15,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -53,7 +53,69 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
+              child: TRoundedImage(
+                imageUrl: TImages.promoBanner3,
+              ),
+            )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class TRoundedImage extends StatelessWidget {
+  const TRoundedImage({
+    super.key,
+    this.width,
+    this.height,
+    required this.imageUrl,
+    this.applyImageRadius = false,
+    this.border,
+    this.backgroundColor = TColors.light,
+    this.fit,
+    this.padding,
+    this.isNetworkImage = false,
+    this.onPressed,
+    this.borderRadius = TSizes.md,
+  });
+
+  final double? width, height;
+  final String imageUrl;
+  final bool applyImageRadius;
+  final BoxBorder? border;
+  final Color backgroundColor;
+  final BoxFit? fit;
+  final EdgeInsetsGeometry? padding;
+  final bool isNetworkImage;
+  final VoidCallback? onPressed;
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: width,
+        height: height,
+        padding: padding,
+        decoration: BoxDecoration(
+          borderRadius: applyImageRadius
+              ? BorderRadius.circular(borderRadius)
+              : BorderRadius.zero,
+        ),
+        child: ClipRRect(
+          borderRadius: applyImageRadius
+              ? BorderRadius.circular(borderRadius)
+              : BorderRadius.zero,
+          child: Image(
+            image: isNetworkImage
+                ? NetworkImage(imageUrl)
+                : AssetImage(imageUrl) as ImageProvider,
+            fit: fit,
+          ),
         ),
       ),
     );
