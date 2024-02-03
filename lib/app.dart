140070cbb07ddf022bac822281c7c41c377604e7
Shahriazshuvo <Shahriaz.Montakim@brainstation-23.com>
2024-02-03 18:21:28 +0600

    set up firebase installation

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'utils/theme/theme.dart';
@@ -13,7 +14,15 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
-      home: const OnBoardingScreen(),
+      home: Scaffold(
+        backgroundColor: TColors.primaryColor,
+        body: Center(
+          child: CircularProgressIndicator(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      // home: const OnBoardingScreen(),
     );
   }
 }
---
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -0,0 +1,28 @@
+import 'package:e_commerce/features/authentication/screens/login/login.dart';
+import 'package:e_commerce/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+
+  /// Variables
+  final deviceStorage = GetStorage();
+
+  /// Call from mainDart from app launch
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    FlutterNativeSplash.remove();
+    screenDirect();
+  }
+
+  /// Function to show relevant screen
+  screenDirect() async {
+    deviceStorage.writeIfNull('IsFirstTime', true);
+    deviceStorage.read('IsFirstTime') != true
+        ? Get.offAll(() => const LoginScreen())
+        : Get.offAll(() => const OnBoardingScreen());
+  }
+}
